b7c001cea78c50f74fd6643fbb633e538404fbd2
anujbajpai459 <anujb9152@gmail.com>
2026-06-24 15:20:40 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger=document.getElementById("hamburger");
+
+const menu=document.getElementById("menu");
+
+hamburger.addEventListener("click",()=>{
+
+if(menu.style.display==="flex"){
+
+menu.style.display="none";
+
+}
+
+else{
+
+menu.style.display="flex";
+
+menu.style.flexDirection="column";
+
+}
+
+});
